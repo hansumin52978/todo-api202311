4416039ed14905bda88c2cd1604ca4c3fea485c1
hansumin52978 <gkstnals2626@naver.com>
2023-11-28 10:43:32 +0900

    #7. User 로그인 기능 구현 (JWT 발급)

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.todo.userapi.service;
 import com.example.todo.auth.TokenProvider;
 import com.example.todo.userapi.dto.request.LoginRequestDTO;
 import com.example.todo.userapi.dto.request.UserRequestSignUpDTO;
+import com.example.todo.userapi.dto.response.LoginResponseDTO;
 import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
 import com.example.todo.userapi.entity.User;
 import com.example.todo.userapi.repository.UserRepository;
@@ -19,7 +20,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
-
     // 회원 가입 처리
     public UserSignUpResponseDTO create(final UserRequestSignUpDTO dto) {
         String email = dto.getEmail();
@@ -46,7 +46,7 @@ public class UserService {
     }
 
     // 회원 인증
-    public void authenticate(final LoginRequestDTO dto) {
+    public LoginResponseDTO authenticate(final LoginRequestDTO dto) {
 
         // 이메일을 통해 회원 정보 조회
         User user = userRepository.findByEmail(dto.getEmail())
@@ -66,5 +66,8 @@ public class UserService {
 
         // 로그인 성공 후에 클라이언트에게 뭘 리턴할 것인가???
         // -> JWT를 클라이언트에게 발급해 주어야 한다.
+        String token = tokenProvider.createToken(user);
+
+        return new LoginResponseDTO(user, token);
     }
 }
