0a5533744a5905af6b0e9ebe86b7a41108ff5118
hansumin52978 <gkstnals2626@naver.com>
2023-11-29 10:47:48 +0900

    #8. JWT 토큰 인증을 활용한 할일 등록 및 목록 불러오기

--- src/main/java/com/example/todo/userapi/dto/request/LoginRequestDTO.java
+++ src/main/java/com/example/todo/userapi/dto/request/LoginRequestDTO.java
@@ -1,6 +1,8 @@
 package com.example.todo.userapi.dto.request;
 
 import lombok.*;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Setter @Getter
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 public class LoginRequestDTO {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
